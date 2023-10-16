ae1e86108e9d1d2efaffd09c00eb584a954cddcf
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2023-10-15 17:19:38 -0700

    Time: 69 ms (5.82%), Space: 42.4 MB (39.68%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,15 +4,13 @@
  */
 var maxProfit = function (prices) {
     let profit = 0;
-
     for (let i = 1; i < prices.length; i++) {
         let prev = prices[i - 1];
         let current = prices[i];
-
-        if (prev < current) {
-            profit += current - prev;
+        
+        if (current > prev) {
+            profit += (current - prev);
         }
     }
-    
     return profit;
 };
